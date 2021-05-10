29aa81dccd280c9bed7be5b55f1fb3a8d3b3cdc5
unknown <L7Buot@gmail.com>
2021-05-10 12:38:43 -0400

    initial commit

---
+++ Controllers/SkillsController.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using sol_Job_Bank.Data;
+using sol_Job_Bank.Models;
+using sol_Job_Bank.Utilities;
+
+namespace sol_Job_Bank.Controllers
+{
+    public class SkillsController : Controller
+    {
+        private readonly JobBankContext _context;
+
+        public SkillsController(JobBankContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Skills
+        [Authorize(Roles = "Admin, Supervisor")]
+        public async Task<IActionResult> Index(int? page, int? pageSizeID)
+        {
+            var skills = _context.Skills
+                .OrderBy(s=>s.Name);
+            //Handle Paging
+            int pageSize;//This is the value we will pass to PaginatedList
+            if (pageSizeID.HasValue)
+            {
+                //Value selected from DDL so use and save it to Cookie
+                pageSize = pageSizeID.GetValueOrDefault();
+                CookieHelper.CookieSet(HttpContext, "pageSizeValue", pageSize.ToString(), 30);
+            }
+            else
+            {
+                //Not selected so see if it is in Cookie
+                pageSize = Convert.ToInt32(HttpContext.Request.Cookies["pageSizeValue"]);
+            }
+            pageSize = (pageSize == 0) ? 3 : pageSize;//Neither Selected or in Cookie so go with default
+            ViewData["pageSizeID"] =
+                new SelectList(new[] { "3", "5", "10", "20", "30", "40", "50", "100", "500" }, pageSize.ToString());
+            var pagedData = await PaginatedList<Skill>.CreateAsync(skills.AsNoTracking(), page ?? 1, pageSize);
+
+            return View(pagedData);
+        }
+
+        // GET: Skills/Details/5
+        [Authorize(Roles = "Admin, Supervisor")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return View(skill);
+        }
+
+        // GET: Skills/Create
+        [Authorize(Roles = "Admin, Supervisor")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Skills/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin, Supervisor")]
+        public async Task<IActionResult> Create([Bind("ID,Name")] Skill skill)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(skill);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+            return View(skill);
+        }
+
+        // GET: Skills/Edit/5
+        [Authorize(Roles = "Admin, Supervisor")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills.FindAsync(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+            return View(skill);
+        }
+
+        // POST: Skills/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin, Supervisor")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var skillToUpdate = await _context.Skills.FindAsync(id);
+            //Check that you got it or exit with a not found error
+            if (skillToUpdate == null)
+            {
+                return NotFound();
+            }
+            if (await TryUpdateModelAsync<Skill>(skillToUpdate, "",
+                o => o.Name))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SkillExists(skillToUpdate.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                }
+
+            }
+            return View(skillToUpdate);
+        }
+
+        // GET: Skills/Delete/5
+        [Authorize(Roles = "Admin, Supervisor")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var skill = await _context.Skills
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return View(skill);
+        }
+
+        // POST: Skills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin, Supervisor")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var skill = await _context.Skills.FindAsync(id);
+            try
+            {
+                _context.Skills.Remove(skill);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException dex)
+            {
+                if (dex.GetBaseException().Message.Contains("FOREIGN KEY constraint failed"))
+                {
+                    ModelState.AddModelError("", "Unable to delete Skill. Remember, you cannot delete a Skill noted for any Applicants.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                }
+            }
+            return View(skill);
+        }
+
+        private bool SkillExists(int id)
+        {
+            return _context.Skills.Any(e => e.ID == id);
+        }
+    }
+}
